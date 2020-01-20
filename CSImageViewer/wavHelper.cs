18251880fc51318783260784e7ccca8464122a06
ggrevera <ggrevera@sju.edu>
2020-01-19 20:29:08 -0500

    upgraded doxygen support files to newest version

--- CSImageViewer/wavHelper.cs
+++ CSImageViewer/wavHelper.cs
@@ -1,5 +1,30 @@
-﻿using System;
-//using System.Collections.Generic;
+﻿/**
+    \file   wavHelper.cs
+    \brief  Contains wavHelper class definition.
+    \author George J. Grevera, Ph.D., ggrevera@sju.edu
+
+    Copyright (C) 2010, George J. Grevera
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+    USA or from http://www.gnu.org/licenses/gpl.txt.
+
+    This General Public License does not permit incorporating this
+    code into proprietary programs.  (So a hypothetical company such
+    as GH (Generally Hectic) should NOT incorporate this code into
+    their proprietary programs.)
+ */
+using System;
 using System.IO;
 using System.Diagnostics;
 //----------------------------------------------------------------------
@@ -8,7 +33,8 @@ using System.Diagnostics;
 
 namespace CSImageViewer {
 
-    /**
+    /** \brief  This class contains methods that read and write wav 
+     * (sound/audio) files.
      */
     class wavHelper {
 
